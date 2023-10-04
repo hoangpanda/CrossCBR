33557495ba72c22a7b6a17f679d5f0ae6202e4a2
hoangpanda <hoang123223@gmail.com>
2023-10-04 09:07:26 +0700

    test

--- train.py
+++ train.py
@@ -254,9 +254,9 @@ def get_metrics(metrics, grd, pred, topks):
     tmp = {"recall": {}, "ndcg": {}}
     for topk in topks:
         _, col_indice = torch.topk(pred, topk)
-        col_indice = col_indice.to(pred.device)
+        col_indice = col_indice.to('cpu')
         row_indice = torch.zeros_like(col_indice) + torch.arange(pred.shape[0], device=pred.device, dtype=torch.long).view(-1, 1)
-        row_indice = row_indice.to(pred.device)
+        row_indice = row_indice.to('cpu')
 
         print('grd: {}'.format(grd))
 
