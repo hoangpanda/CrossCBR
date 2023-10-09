1fd141330f91afdcb4de84b38e7ee98462d988e6
hoangpanda <hoang123223@gmail.com>
2023-10-09 16:58:28 +0700

    t

--- models/CrossCBR.py
+++ models/CrossCBR.py
@@ -264,10 +264,11 @@ class CrossCBR(nn.Module):
         b_cross_view_cl = self.cal_c_loss(IL_bundles_feature, BL_bundles_feature)
 
         c_losses = [u_cross_view_cl, b_cross_view_cl]
-        alpha_c_loss = 0.1
+        
        # c_loss = u_cross_view_cl*alpha_c_loss + b_cross_view_cl*(1-alpha_c_loss)
        # c_loss = c_loss*0.5
-        c_loss = sum(c_losses) / len(c_losses)
+        alpha_c_loss = 0.8
+        c_loss = (u_cross_view_cl*alpha_c_loss + (1-alpha_c_loss)*b_cross_view_cl) / len(c_losses)
 
         return bpr_loss, c_loss
 
