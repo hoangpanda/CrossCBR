5e7318c7eeece70075870c089c679b3b9f03ed65
hoangpanda <hoang123223@gmail.com>
2023-10-11 14:29:44 +0700

    tét

--- models/CrossCBR.py
+++ models/CrossCBR.py
@@ -35,7 +35,6 @@ def to_tensor(graph):
     values = graph.data
     indices = np.vstack((graph.row, graph.col))
     graph = torch.sparse.FloatTensor(torch.LongTensor(indices), torch.FloatTensor(values), torch.Size(graph.shape))
-
     return graph
 
 
@@ -43,6 +42,82 @@ def np_edge_dropout(values, dropout_ratio):
     mask = np.random.choice([0, 1], size=(len(values),), p=[dropout_ratio, 1-dropout_ratio])
     values = mask * values
     return values
+
+
+class GATLayer(nn.Module):
+
+    def __init__(self, c_in, c_out, num_heads=1, concat_heads=True, alpha=0.2):
+        """
+        Inputs:
+            c_in - Dimensionality of input features
+            c_out - Dimensionality of output features
+            num_heads - Number of heads, i.e. attention mechanisms to apply in parallel. The
+                        output features are equally split up over the heads if concat_heads=True.
+            concat_heads - If True, the output of the different heads is concatenated instead of averaged.
+            alpha - Negative slope of the LeakyReLU activation.
+        """
+        super().__init__()
+        self.num_heads = num_heads
+        self.concat_heads = concat_heads
+        if self.concat_heads:
+            assert c_out % num_heads == 0, "Number of output features must be a multiple of the count of heads."
+            c_out = c_out // num_heads
+
+        # Sub-modules and parameters needed in the layer
+        self.projection = nn.Linear(c_in, c_out * num_heads)
+        self.a = nn.Parameter(torch.Tensor(num_heads, 2 * c_out)) # One per head
+        self.leakyrelu = nn.LeakyReLU(alpha)
+
+        # Initialization from the original implementation
+        nn.init.xavier_uniform_(self.projection.weight.data, gain=1.414)
+        nn.init.xavier_uniform_(self.a.data, gain=1.414)
+
+    def forward(self, node_feats, adj_matrix, print_attn_probs=False):
+        """
+        Inputs:
+            node_feats - Input features of the node. Shape: [batch_size, c_in]
+            adj_matrix - Adjacency matrix including self-connections. Shape: [batch_size, num_nodes, num_nodes]
+            print_attn_probs - If True, the attention weights are printed during the forward pass (for debugging purposes)
+        """
+        batch_size, num_nodes = node_feats.size(0), node_feats.size(1)
+
+        # Apply linear layer and sort nodes by head
+        node_feats = self.projection(node_feats)
+        node_feats = node_feats.view(batch_size, num_nodes, self.num_heads, -1)
+
+        # We need to calculate the attention logits for every edge in the adjacency matrix
+        # Doing this on all possible combinations of nodes is very expensive
+        # => Create a tensor of [W*h_i||W*h_j] with i and j being the indices of all edges
+        edges = adj_matrix.nonzero(as_tuple=False) # Returns indices where the adjacency matrix is not 0 => edges
+        node_feats_flat = node_feats.view(batch_size * num_nodes, self.num_heads, -1)
+        edge_indices_row = edges[:,0] * num_nodes + edges[:,1]
+        edge_indices_col = edges[:,0] * num_nodes + edges[:,2]
+        a_input = torch.cat([
+            torch.index_select(input=node_feats_flat, index=edge_indices_row, dim=0),
+            torch.index_select(input=node_feats_flat, index=edge_indices_col, dim=0)
+        ], dim=-1) # Index select returns a tensor with node_feats_flat being indexed at the desired positions along dim=0
+
+        # Calculate attention MLP output (independent for each head)
+        attn_logits = torch.einsum('bhc,hc->bh', a_input, self.a)
+        attn_logits = self.leakyrelu(attn_logits)
+
+        # Map list of attention values back into a matrix
+        attn_matrix = attn_logits.new_zeros(adj_matrix.shape+(self.num_heads,)).fill_(-9e15)
+        attn_matrix[adj_matrix[...,None].repeat(1,1,1,self.num_heads) == 1] = attn_logits.reshape(-1)
+
+        # Weighted average of attention
+        attn_probs = F.softmax(attn_matrix, dim=2)
+        if print_attn_probs:
+            print("Attention probs\n", attn_probs.permute(0, 3, 1, 2))
+        node_feats = torch.einsum('bijh,bjhc->bihc', attn_probs, node_feats)
+
+        # If heads should be concatenated, we can do this by reshaping. Otherwise, take mean
+        if self.concat_heads:
+            node_feats = node_feats.reshape(batch_size, num_nodes, -1)
+        else:
+            node_feats = node_feats.mean(dim=2)
+
+        return node_feats
 
 
 class CrossCBR(nn.Module):
@@ -185,24 +260,31 @@ class CrossCBR(nn.Module):
         # print(f'A_feature: {A_feature}')
         # print(f'B_feature shape: {B_feature.shape}')
         # print(f'B_feature: {B_feature}')
-        features = torch.cat((A_feature, B_feature), 0)
-        all_features = [features]
 
-        for i in range(self.num_layers):
-            # spmm <=> torch.sparse.mm -> multiply two matrix
-            features = torch.spmm(graph, features)
-            if self.conf["aug_type"] == "MD" and not test: # !!! important
-                features = mess_dropout(features)
+        layer = GATLayer(64, 64, num_heads=2)
 
-            features = features / (i+2)
-            all_features.append(F.normalize(features, p=2, dim=1))
+        indices = graph._indices()
+        A_feature = layer(A_feature, indices, print_attn_probs=True)
+        B_feature = layer(B_feature, indices, print_attn_probs=True)
+        # features = torch.cat((A_feature, B_feature), 0)
+        # all_features = [features]
+        
+        # for i in range(self.num_layers):
+        #     # spmm <=> torch.sparse.mm -> multiply two matrix
+        #     features = torch.spmm(graph, features)
+        #     if self.conf["aug_type"] == "MD" and not test: # !!! important
+        #         features = mess_dropout(features)
 
-        all_features = torch.stack(all_features, 1)
-        all_features = torch.sum(all_features, dim=1).squeeze(1)
+        #     features = features / (i+2)
+        #     all_features.append(F.normalize(features, p=2, dim=1))
 
-        A_feature, B_feature = torch.split(all_features, (A_feature.shape[0], B_feature.shape[0]), 0)
+        # all_features = torch.stack(all_features, 1)
+        # all_features = torch.sum(all_features, dim=1).squeeze(1)
+
+        # A_feature, B_feature = torch.split(all_features, (A_feature.shape[0], B_feature.shape[0]), 0)
 
         return A_feature, B_feature
+
 
 
     def get_IL_bundle_rep(self, IL_items_feature, test):
